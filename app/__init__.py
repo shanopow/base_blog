8e82f62f27f31b855c2a7362090efc649572a1ff
shanopow <115043900+shanopow@users.noreply.github.com>
2023-01-18 15:47:54 +0000

    Reverted Back to good version

--- app/__init__.py
+++ app/__init__.py
@@ -4,25 +4,12 @@ import os
 from flask import Flask, request, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
-from flask_mail import Mail
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from flask_babel import Babel, lazy_gettext as _l
-from elasticsearch import Elasticsearch
-from redis import Redis
-import rq
 from config import Config
 
-db = SQLAlchemy()
-migrate = Migrate()
-login = LoginManager()
-login.login_view = 'auth.login'
-login.login_message = _l('Please log in to access this page.')
-mail = Mail()
-bootstrap = Bootstrap()
-moment = Moment()
-babel = Babel()
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 def create_app(config_class=Config):
